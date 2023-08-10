24bee5b8c9bb74ea4f16daca6e5333100920173c
Khabir Ahmed <khabirahmed780@gmail.com>
2023-08-10 23:43:05 +0530

    first commit

---
+++ models/taskModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  reminder: {
+    type: Date,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  user_id: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Task', taskSchema)
